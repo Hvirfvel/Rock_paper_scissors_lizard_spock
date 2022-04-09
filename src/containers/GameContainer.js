e58dc4795bd1cde2fd78c817472371aeedd691c6
hvirfvel <carolinehvirfvel@hotmail.com>
2022-04-09 13:02:48 +0100

    sets computerHand on play click

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HandList from "../components/HandList";
 
 const GameContainer = () => {
 
     const [userHand, setUserHand] = useState("");
+    const [computerHand, setComputerHand] = useState("");
+    const [playButton, setPlayButton] = useState(false);
+
+    useEffect(() => {
+        getComputerHand();
+    }, [playButton])
 
     const hands = ["rock", "paper", "scissors", "lizard", "spock"];
 
@@ -11,10 +17,21 @@ const GameContainer = () => {
         setUserHand(hand);
     }
 
+    const handlePlayClick = () => {
+        setPlayButton(true);
+    }
+
+    const getComputerHand = () => {
+        const randomHandIndex = Math.floor(Math.random() * hands.length);
+        const randomHand = hands[randomHandIndex];
+        setComputerHand(randomHand);
+    }
+
     return (
         <>
             <h2>GameContainer</h2>
             <HandList hands={hands} onHandClick={onHandClick}/>
+            <button onClick={handlePlayClick}>Play</button>
         </>
     )
 }
