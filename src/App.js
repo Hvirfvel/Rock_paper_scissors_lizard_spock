84411e702b461ac5d9a4009381dad904a5c248ab
hvirfvel <carolinehvirfvel@hotmail.com>
2022-04-09 11:46:44 +0100

    added GameContainer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import GameContainer from './containers/GameContainer';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <GameContainer/>
+    </>
   );
 }
 
---
+++ src/containers/GameContainer.js
@@ -0,0 +1,10 @@
+const GameContainer = () => {
+
+    return (
+        <>
+            <h2>GameContainer</h2>
+        </>
+    )
+}
+
+export default GameContainer;
